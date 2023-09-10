ccf512dc81b2981b19544d04a46d1a173b751a91
Ibnu Sina <ibenu.sina@gmail.com>
2023-09-10 22:37:19 +0800

    first commit

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,30 @@
+import 'package:meals_app/models/category.dart';
+import 'package:flutter/material.dart';
+
+const availableCategoryies = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.purple,
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/categories.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const Scaffold(
+        body: Center(
+          child: CategoriesScreen(),
+        ),
+      ),
+    );
+  }
+}
